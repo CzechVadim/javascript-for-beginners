741263343c55a0b2944ef7c0d67adddce0aef549
CzechVadim <czech_vadim@ukr.net>
2021-06-06 00:58:59 +0300

    Закончил изучать 3. Математические операции над числами в JavaScript

---
+++ js/mathematical-operations.js
@@ -0,0 +1,70 @@
+/*let sumNumbers = 1 + 2 + 3;
+console.log(sumNumbers);*/
+
+/* Вывод значения без ввода переменной */
+//console.log(1 + 2 + 3);
+
+
+
+/*let numberOne = 10;
+let numberTwo = 2;
+console.log(10 + 2);
+console.log(10 - 2);
+console.log(10 * 2);
+console.log(10 / 2);
+
+
+
+let numberThree = 10;
+let numberFour = 5;
+let result = numberThree + numberFour;
+console.log(result);
+
+
+
+let number1 = 1;
+let number2 = 2;
+let number3 = 3;
+console.log(1 + 2 +3);
+
+
+
+let number10 = 10;
+let number5 = 5;
+let subtraction = number10 - number5;
+
+let number7 = 7;
+
+let resultAddition = subtraction + number7;
+console.log(resultAddition);*/
+
+
+/*let sum = 2 * (8 / 4);
+console.log(sum);*/
+
+
+
+let numberA = 1.5;
+let numberB = 0.75;
+let sumNumbers = numberA + numberB;
+console.log(sumNumbers);  //2.25
+
+
+let positiveNumber = 100;
+let negativeNumber = -positiveNumber;
+console.log(negativeNumber);
+
+
+
+let power = 3 * 2 ** 3 * 3;
+console.log(power); //72
+
+
+
+const pricePerItem = 3500;
+const orderedQuantity = 4;
+const totalPrice = pricePerItem * orderedQuantity; /* операнд pricePerItem омножается на операнд orderedQuantity с помощью оператора *. */
+console.log(totalPrice);
+
+
+
--- js/variables.js
+++ js/variables.js
@@ -1,3 +1,4 @@
+/* <ключевое слово> <имя переменной> = <значение> */
 let num = 123;   //Обьявление переменной с помощью ключевого слова let под именем num к которой присваивается с помощью операции присваивания = значение 123
 console.log(num);   //Вывод содержимого переменной
 
@@ -10,3 +11,31 @@ console.log(a);   //Вывод содержимого переменной
 a = 20;   //Присваивания переменной a значения 20
 console.log(a);   //Вывод содержимого переменной
 
+
+
+/*const productName = "Droid";
+const pricePerItem = 2000;
+console.log(productName);
+console.log(pricePerItem);*/
+
+
+/* Изменения значений переменных */
+/*let productName = "Droid";
+let pricePerItem = 2000;
+productName = "Repair droid";
+pricePerItem = 3500;
+console.log(productName);
+console.log(pricePerItem);
+
+
+
+let topSpeed = 160;
+let distance = 617.54;
+let login = "mango935";
+let isOnline = true;
+let isAdmin = false;
+console.log(topSpeed);
+console.log(distance);
+console.log(login);
+console.log(isOnline);
+console.log(isAdmin);*/
